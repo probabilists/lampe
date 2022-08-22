575ed44d974036ae3f29d783213b5f2dc65441ae
François Rozet <francois.rozet@outlook.com>
2022-08-22 11:40:35 +0200

    🧑‍💻 Add push flag to sphinx build

--- sphinx/build.sh
+++ sphinx/build.sh
@@ -3,9 +3,18 @@
 set -e
 shopt -s globstar
 
+while getopts p flag
+do
+	case "${flag}" in
+		p) push=true;;
+	esac
+done
+
 # Merge
-git checkout docs
-git merge master -m "🔀 Merge master into docs"
+if [ $push ]; then
+	git checkout docs
+	git merge master -m "🔀 Merge master into docs"
+fi
 
 # Generate HTML
 sphinx-build -b html . ../docs
@@ -20,5 +29,7 @@ sed "s|\(<a class=\"reference external\".*</a>\)\(<a class=\"headerlink\".*</a>\
 sed "s|<a class=\"muted-link\" href=\"https://pradyunsg.me\">@pradyunsg</a>'s||g" -i **/*.html
 
 # Push
-git add .
-git commit -m "📝 Update Sphinx documentation"
+if [ $push ]; then
+	git add .
+	git commit -m "📝 Update Sphinx documentation"
+fi
